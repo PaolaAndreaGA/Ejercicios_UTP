cd90dc641fe35b2c7dae6ed83244f08a6cb09d9e
PaolaAndreaGA <paga0209@gmial.com>
2022-05-05 08:53:26 -0500

    reto semana 1

--- reto_semana1.py
+++ reto_semana1.py
@@ -1,4 +1,3 @@
-
 def CDT(usuario:str, capital: int, tiempo: int):
     """
         Función que reciba como parámetros:
@@ -11,19 +10,15 @@ def CDT(usuario:str, capital: int, tiempo: int):
 
     if tiempo > 2:
         porc_int = 0.03
-        valor_int = int(capital * porc_int * tiempo)/12
-        valor_total = int(capital + valor_int)
+        valor_int = (capital * porc_int * tiempo)/12
+        valor_total = (capital + valor_int)
     else:
         penalizacion = 0.02
         valor_perd = capital * penalizacion
         valor_total = capital - valor_perd
 
 
-    return valor_total
+    return f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {valor_total}"
 
-usuario = input("Digite su nombre de usuario: ")
-capital = int(input("Digite el valor de su capital: "))
-tiempo = int(input("Digite tiempo: "))
-info_cdt = CDT(usuario, capital, tiempo)
-
-print('Para el usuario {}', usuario, 'La cantidad de dinero a recibir, según el monto inicial {} ', capital, 'para un tiempo de ', tiempo, 'meses es: {}', info_cdt)
+print(CDT("AB1012",1000000,3))
+print(CDT("QW3456",5000000,2))
---
+++ reto_semana1_input.py
@@ -0,0 +1,30 @@
+
+def CDT(usuario: str, capital: int, tiempo: int):
+    """
+        Función que reciba como parámetros:
+        usuario: usuario (str)
+        capital: monto a ingresar (int)
+        tiempo: tiempo del CDT (int)
+        Retorna: Una cadena de caracteres que le proporcione
+        al banco la información que desea obtener.
+    """
+
+    if tiempo > 2:
+        porc_int = 0.03
+        valor_int = int(capital * porc_int * tiempo)/12
+        valor_total = int(capital + valor_int)
+    else:
+        penalizacion = 0.02
+        valor_perd = capital * penalizacion
+        valor_total = capital - valor_perd
+
+
+    return valor_total
+
+usuario = input("Digite su nombre de usuario: ")
+capital = int(input("Digite el valor de su capital: "))
+tiempo = int(input("Digite tiempo: "))
+info_cdt = CDT(usuario, capital, tiempo)
+
+
+print(f"Para el usuario {usuario} La cantidad de dinero a recibir, según el monto inicial {capital} para un tiempo de {tiempo} meses es: {info_cdt}")
