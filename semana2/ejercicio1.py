dc9fa978a22e2d6fca2b179301a27161961120e1
PaolaAndreaGA <paga0209@gmial.com>
2022-05-12 08:20:07 -0500

    ejercicio1.py

--- semana2/ejercicio1.py
+++ semana2/ejercicio1.py
@@ -2,12 +2,21 @@
 #El algoritmo debe imprimir el valor ingresado, y de ser una nota mayor o igual a 4.0,
 #deberá imprimir un mensaje de felicitaciones.
 
-nota = float(input("ingrese su nota entre 0.0 y 5.0: "))
-if(nota >= 0.0 and nota <= 5.0):
-  if(nota >= 4.0):
-    print (f"su nota es: {nota:.1f} ¡Felicitaciones!")
-  else:
-    print (f"su nota es: {nota:.1f}, esfuerzate un poco más, ¡tú puedes!")
-else:
-  print (f"la nota : {nota:.1f} está fuera del rango, por favor ingrese una nota entre 0.0 y 5.0")
+def nota_definitiva(nota: float):
 
+    if not isinstance(nota, float) and not isinstance(nota, int):
+        return ("la nota debe ser un decimal")
+
+
+    if(nota < 0.0 or nota > 5.0):
+        return (f"la nota : {nota:.1f} está fuera del rango, por favor ingrese una nota entre 0.0 y 5.0")
+
+    if(nota >= 4.0):
+        mensaje = (f"su nota es: {nota:.1f} ¡Felicitaciones!")
+    else:
+        mensaje  = (f"su nota es: {nota:.1f}, esfuerzate un poco más, ¡tú puedes!")
+    return mensaje
+
+nota = float(input("ingrese su nota entre 0.0 y 5.0: "))
+mensaje_final = nota_definitiva
+print(mensaje_final(nota))
