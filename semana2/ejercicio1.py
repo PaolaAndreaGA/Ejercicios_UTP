4d597ac379d620cbebe7c6e115b5b1b8ea5ac9df
PaolaAndreaGA <paga0209@gmial.com>
2022-05-12 08:30:57 -0500

    ejercicio1.py

--- semana2/ejercicio1.py
+++ semana2/ejercicio1.py
@@ -7,9 +7,9 @@ def nota_definitiva(nota: float):
     if not isinstance(nota, float) and not isinstance(nota, int):
         return ("la nota debe ser un decimal")
 
-
     if(nota < 0.0 or nota > 5.0):
         return (f"la nota : {nota:.1f} está fuera del rango, por favor ingrese una nota entre 0.0 y 5.0")
+
 
     if(nota >= 4.0):
         mensaje = (f"su nota es: {nota:.1f} ¡Felicitaciones!")
@@ -18,5 +18,4 @@ def nota_definitiva(nota: float):
     return mensaje
 
 nota = float(input("ingrese su nota entre 0.0 y 5.0: "))
-mensaje_final = nota_definitiva
-print(mensaje_final(nota))
+print(nota_definitiva(nota))
