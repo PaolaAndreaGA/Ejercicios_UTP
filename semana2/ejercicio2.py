59722d29b61cdb3f9f41508bf2369d0062962c30
PaolaAndreaGA <paga0209@gmial.com>
2022-05-12 08:30:23 -0500

    ejercicio2.py

--- semana2/ejercicio2.py
+++ semana2/ejercicio2.py
@@ -2,11 +2,21 @@
 # El algoritmo debe imprimir el valor ingresado, y un mensaje que indique si el estudiante “Ganó el curso”
 # o “Perdió el curso”. Se gana el curso solo si la nota definitiva es mayor o igual a 3.0..
 
+
+def nota_definitiva(nota: float):
+
+    if not isinstance(nota, float) and not isinstance(nota, int):
+      return ("la nota debe ser un decimal")
+
+    if(nota < 0.0 or nota > 5.0):
+      return (f"la nota : {nota:.1f} está fuera del rango, por favor ingrese una nota entre 0.0 y 5.0")
+
+    if(nota >= 3.0):
+      mensaje = (f"su nota es: {nota} ¡Felicitaciones usted ganó el curso!")
+    else:
+      mensaje = (f"su nota es: {nota}, lo sentimos, usted ha perdido el curso")
+    return mensaje
+
+
 nota = float(input("ingrese su nota entre 0.0 y 5.0: "))
-if(nota >= 0.0 and nota <= 5.0):
-  if(nota >= 3.0):
-    print (f"su nota es: {nota} ¡Felicitaciones usted ganó el curso!")
-  else:
-    print (f"su nota es: {nota}, lo sentimos, usted ha perdido el curso")
-else:
-  print (f"la nota : {nota} está fuera del rango, por favor ingrese una nota entre 0.0 y 5.0") 
+print(nota_definitiva(nota))
