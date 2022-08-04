aaecf2a82fffdeb5bc77012bd0967916214afe40
Paola Andrea García Altamirano <85592476+PaolaAndreaGA@users.noreply.github.com>
2022-08-04 07:03:05 -0500

    Update Computadores.java
    Changes on file

--- ciclo2/semana3/Computadores.java
+++ ciclo2/semana3/Computadores.java
@@ -1,158 +1,175 @@
 public class Computadores {
-  // Variables base
-  // Variables
-  // Constructores public Computadores(){
-  // Constructor
-  public Computadores() {
-  }
 
-  public Computadores(Double precioBase, Integer peso) {
-  }
+    protected final static char CONSUMO_W = 'F';
+    protected final static Double PRECIO_BASE = 100.0;
+    protected final static Integer PESO_BASE = 5;
 
-  public Computadores(Double precioBase, Integer peso, char consumoW) {
-    this.precioBase = precioBase;
-    this.peso = peso;
-    this.consumoW = consumoW;
-  }
+    protected Double precioBase;
+    protected char consumoW;
+    protected Integer peso;
 
-  // Metodos
-  public Double calcularPrecio(char consumoW, Integer peso) {
-    Double adicion = 0.0;
-    if (consumoW == 'A') {
-      return adicion = 100.0;
-    } else if (consumoW == 'B') {
-      return adicion = 80.0;
-    } else if (consumoW == 'C') {
-      return adicion = 60.0;
-    } else if (consumoW == 'D') {
-      return adicion = 50.0;
-    } else if (consumoW == 'E') {
-      return adicion = 30.0;
-    } else if (consumoW == 'F') {
-      return adicion = 10.0;
+    // Constructores
+    public Computadores() {
+        this(PRECIO_BASE, PESO_BASE, CONSUMO_W);
     }
 
-    if ((peso >= 0) & (peso < 19)) {
-      return adicion + 10.0;
-    } else if ((peso >= 20) & (peso < 49)) {
-      return adicion + 50.0;
-    } else if ((peso >= 50) & (peso < 79)) {
-      return adicion + 80.0;
-    } else if (peso >= 80) {
-      return adicion + 100.0;
-    } else {
-      return adicion = 0.0;
+    public Computadores(Double precioBase, Integer peso) {
+        this(precioBase, peso, CONSUMO_W);
     }
 
-  }
+    public Computadores(Double precioBase, Integer peso, char consumoW) {
+        this.precioBase = precioBase;
+        this.peso = peso;
+        this.consumoW = consumoW;
+    }
 
-  // Getter
-  public class ComputadoresMesa extends Computadores {
+    // Metodos
+    public Double calcularPrecio() {
+        Double adicion = 0.0;
+        switch (consumoW) {
+            case 'A':
+                adicion += 100.0;
+                break;
+            case 'B':
+                adicion += 80.0;
+                break;
+            case 'C':
+                adicion += 60.0;
+                break;
+            case 'D':
+                adicion += 50.0;
+                break;
+            case 'E':
+                adicion += 30.0;
+                break;
+            case 'F':
+                adicion += 10.0;
+                break;
+        }
+
+        if (peso >= 0 && peso < 19) {
+            adicion += 10.0;
+        } else if (peso >= 20 && peso < 49) {
+            adicion += 50.0;
+        } else if (peso >= 50 && peso <= 79) {
+            adicion += 80.0;
+        } else if (peso >= 80) {
+            adicion += 100.0;
+        }
+
+        return precioBase + adicion;
+    }
+
+    public Double getPrecioBase() {
+        return precioBase;
+    }
+
+    public char getconsumoW() {
+        return consumoW;
+    }
+
+    public Integer getPeso() {
+        return peso;
+    }
+}
+
+public class ComputadoresMesa extends Computadores {
     private final static Integer ALMACENAMIENTO_BASE = 50;
 
-    // Variable
+    private Integer almacenamiento;
+
     // Constructor
     public ComputadoresMesa() {
+        this(PRECIO_BASE, PESO_BASE, CONSUMO_W, ALMACENAMIENTO_BASE);
     }
 
-    // Constructor
     public ComputadoresMesa(Double precioBase, Integer peso) {
+        this(precioBase, peso, CONSUMO_W, ALMACENAMIENTO_BASE);
     }
 
-    // Constructor
     public ComputadoresMesa(Double precioBase, Integer peso, char consumoW, Integer almacenamiento) {
+        super(precioBase, peso, consumoW);
+        this.almacenamiento = almacenamiento;
     }
 
     // Métodos
-    public Double calcularPrecio(Double adicion, Integer almacenamiento) {
-      // Código return adicion;
-      almacenamiento = ALMACENAMIENTO_BASE;
-      if (almacenamiento > 100) {
-        return adicion + 50.0;
-      } else {
+    public Double calcularPrecio() {
+        Double adicion = super.calcularPrecio();
+        if (almacenamiento > 100) {
+            adicion += 50.0;
+        }
+
         return adicion;
-      }
     }
 
-    public Integer getCarga(Integer almacenamiento) {
-      return almacenamiento;
+    public Integer getCarga() {
+        return almacenamiento;
     }
-  }
+}
 
-  public class ComputadoresPortatiles extends Computadores {
+public class ComputadoresPortatiles extends Computadores {
     private final static Integer PULGADAS_BASE = 20;
+
     private Integer pulgadas;
     private boolean camaraITG;
 
     // Constructor
     public ComputadoresPortatiles() {
+        this(PRECIO_BASE, PESO_BASE, CONSUMO_W, PULGADAS_BASE, false);
     }
 
-    // Constructor
     public ComputadoresPortatiles(Double precioBase, Integer peso) {
+        this(precioBase, peso, CONSUMO_W, PULGADAS_BASE, false);
     }
 
-    // Constructor
-    public ComputadoresPortatiles(Double precioBase, Integer peso, char consumoW, Integer pulgadas,
-        boolean camaraITG) {
+    public ComputadoresPortatiles(Double precioBase, Integer peso, char consumoW, Integer pulgadas, boolean camaraITG) {
+        super(precioBase, peso, consumoW);
+        this.pulgadas = pulgadas;
+        this.camaraITG = camaraITG;
     }
 
     // Métodos
-    public Double calcularPrecio(Double adicion, Double precioBase) {
-      // Método Código return adicion;
-      if (pulgadas > 40) {
-        adicion = adicion + (precioBase * 0.3);
-        if (camaraITG == true) {
-          adicion = adicion + (precioBase * 0.5);
+    public Double calcularPrecio() {
+        Double adicion = super.calcularPrecio();
+        if (pulgadas > 40) {
+            adicion += precioBase * 0.3;
         }
-        return adicion;
-      } else {
-        return adicion;
-      }
-    }
-  }
+        if (camaraITG) {
+            adicion += 50.0;
+        }
 
-  public class PrecioTotal {
+        return adicion;
+    }
+}
+
+public class PrecioTotal {
     private Double totalComputadores = 0.0;
     private Double totalComputadoresPortatiles = 0.0;
     private Double totalComputadoresMesa = 0.0;
     private Computadores[] listaComputadores;
 
     // Constructor
-    PrecioTotal(Computadores[] pComputadores) {
-      this.listaComputadores = pComputadores;
+    PrecioTotal(Computadores[] pComputadores){
+        this.listaComputadores = pComputadores;
     }
 
     public void mostrarTotales() {
-      // Código
-      // Mostramos los resultados
-      System.out.println("La suma del precio de los computadores es de " + totalComputadores);
-      System.out.println("La suma del precio de los computadores de mesa es de " + totalComputador);
-      System.out.print("La suma del precio de los computadores portátiles es de " + totalComputadoresPortatiles);
-    }
-  }
-}
+        for (int i = 0; i < listaComputadores.length; i++) {
 
-public class Main {
-public static void main(String[] args) {
-// Pruebas Públicas
-Computadores computadores[] = new Computadores[6];
-computadores[0] = new Computadores(150.0, 70, 'A');
-computadores[1] = new ComputadoresMesa(70.0, 40);
-computadores[2] = new ComputadoresPortatiles(600.0, 70, 'D', 50, false);
-computadores[3] = new Computadores();
-computadores[4] = new Computadores(500.0, 60, 'A');
-computadores[5] = new Computadores(700.0, 50, 'D');
-PrecioTotal solucion1 = new PrecioTotal(computadores);
-solucion1.mostrarTotales();
-System.out.println();
-Computadores computadores[] = new Computadores[4];
-computadores[0] = new Computadores(60.0, 10, 'D');
-computadores[1] = new ComputadoresMesa(300.0, 40, 'Z', 40);
-computadores[2] = new ComputadoresPortatiles(50.0, 10, 'A', 70, false);
-computadores[3] = new Computadores(50.0, 10);
-PrecioTotal solucion1 = new PrecioTotal(computadores);
-solucion1.mostrarTotales();
-System.out.println()
-}
+            if (listaComputadores[i] instanceof Computadores) {
+                totalComputadores += listaComputadores[i].calcularPrecio();
+            }
+            if (listaComputadores[i] instanceof ComputadoresMesa) {
+                totalComputadoresMesa += listaComputadores[i].calcularPrecio();
+            }
+            if (listaComputadores[i] instanceof ComputadoresPortatiles) {
+                totalComputadoresPortatiles += listaComputadores[i].calcularPrecio();
+            }
+        }
+
+        // Mostramos los resultados
+        System.out.println("La suma del precio de los computadores es de " + totalComputadores);
+        System.out.println("La suma del precio de los computadores de mesa es de " + totalComputadoresMesa);
+        System.out.print("La suma del precio de los computadores portátiles es de " + totalComputadoresPortatiles);
+    }
 }
